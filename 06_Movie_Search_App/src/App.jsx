2c5046a8a5840a8bc54f6073d89ffc10a667d3ab
Muhammad Afzal Bhatti <iffiafzal@gmail.com>
2026-04-13 18:47:51 +0500

    Rename and refactor MovieSearchApp to SearchMovieApp

--- 06_Movie_Search_App/src/App.jsx
+++ 06_Movie_Search_App/src/App.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
+import './App.css'
 
-export default function MovieSearchApp(){
+export default function SearchMovieApp(){
+
+  const API_KEY = '2f99e84e';
 
   const [searchQuery, setSearchQuery] = useState('');
   const [movies, setMovies] = useState([]);
 
-  const API_KEY = '2f99e84e';
-
-  const searchMovie = async ()=>{
-
+  const searchMovies = async ()=>{
     if(!searchQuery) return;
 
     const response = await fetch(`http://www.omdbapi.com/?s=${searchQuery}&apikey=${API_KEY}`);
@@ -16,23 +16,22 @@ export default function MovieSearchApp(){
 
     console.log(data);
 
-    if(data.Response === "True"){
+    if(data.Response === 'True'){
       setMovies(data.Search);
     }
   }
 
-
   return(
     <>
-    <div>
-    <h3>Movie Search App </h3>
-    <input type="text" placeholder="Enter Movie Name..." value={searchQuery} onChange={(e)=> setSearchQuery(e.target.value)} />
-    <button onClick={searchMovie}> Search </button>
+    <div className="container">
+    <h3> Search Movie App </h3>
+    <input type="text" placeholder="Enter Movie Name..." value={searchQuery} onChange={(e)=>setSearchQuery(e.target.value)} />
+    <button onClick={searchMovies}>Search </button>
     <div>
       {movies.map((item, index)=>(
-        <div key={index}>
+        <div key={index} className="movie-continer">
           <img src={item.Poster} alt={item.Title} />
-          <h3>{item.Title}</h3>
+          <h2>{item.Title}</h2>
           <p>{item.Year}</p>
         </div>
       ))}
@@ -40,5 +39,4 @@ export default function MovieSearchApp(){
     </div>
       </>
   )
-
 }
