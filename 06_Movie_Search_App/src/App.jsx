6290a385b5206e27233c505488c7459f656b8835
Muhammad Afzal Bhatti <iffiafzal@gmail.com>
2026-04-14 10:36:11 +0500

    Refactor Movie Search App component and logic

--- 06_Movie_Search_App/src/App.jsx
+++ 06_Movie_Search_App/src/App.jsx
@@ -1,14 +1,27 @@
-import React, { useState } from "react";
-import './App.css'
+import React, { useState, useEffect } from "react";
+import './App.css';
 
-export default function SearchMovieApp(){
+export default function MovieSearchApp(){
 
-  const API_KEY = '2f99e84e';
+   const API_KEY = '2f99e84e';
 
   const [searchQuery, setSearchQuery] = useState('');
   const [movies, setMovies] = useState([]);
 
-  const searchMovies = async ()=>{
+
+  useEffect(()=>{
+    const saveMovies = localStorage.getItem('movies')
+    setMovies(JSON.parse(saveMovies))
+  }, []);  
+
+  useEffect(()=>{
+    if(movies.length > 0){
+      localStorage.setItem('movies', JSON.stringify(movies))
+    }
+  }, [movies])
+
+  const searchMovie = async ()=>{
+
     if(!searchQuery) return;
 
     const response = await fetch(`http://www.omdbapi.com/?s=${searchQuery}&apikey=${API_KEY}`);
@@ -16,22 +29,23 @@ export default function SearchMovieApp(){
 
     console.log(data);
 
-    if(data.Response === 'True'){
+    if(data.Response === "True"){
       setMovies(data.Search);
     }
   }
 
+
   return(
     <>
-    <div className="container">
-    <h3> Search Movie App </h3>
-    <input type="text" placeholder="Enter Movie Name..." value={searchQuery} onChange={(e)=>setSearchQuery(e.target.value)} />
-    <button onClick={searchMovies}>Search </button>
+    <div>
+    <h3>Movie Search App </h3>
+    <input type="text" placeholder="Enter Movie Name..." value={searchQuery} onChange={(e)=> setSearchQuery(e.target.value)} />
+    <button onClick={searchMovie}> Search </button>
     <div>
       {movies.map((item, index)=>(
-        <div key={index} className="movie-continer">
+        <div key={index}>
           <img src={item.Poster} alt={item.Title} />
-          <h2>{item.Title}</h2>
+          <h3>{item.Title}</h3>
           <p>{item.Year}</p>
         </div>
       ))}
@@ -39,4 +53,5 @@ export default function SearchMovieApp(){
     </div>
       </>
   )
+
 }
